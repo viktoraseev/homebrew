be6d1275c89ac0a84f2d4f19126b4770e5397a32
viktoraseev <viktoraseev@yandex-team.ru>
2026-09-17 09:36:41 +0300

    Add DNS Reset for Tunnelblick cask

---
+++ Casks/dns-reset-tunnelblick.rb
@@ -0,0 +1,16 @@
+cask "dns-reset-tunnelblick" do
+  version "1.0.0"
+  arch arm: "arm64", intel: "x86_64"
+
+  sha256 arm:   "8703c18e7b86dff22c643db8a9ca70fce058a96304da89cb0d2e9ef11ca9b6fa",
+         intel: "eb12f22f9b41b36325a338bd9dafa6c57dd08eeefb26a0d1b23840b4da68effd"
+
+  url "https://github.com/viktoraseev/dns-reset-tunnelblick/releases/download/v#{version}/dns-reset-tunnelblick-v#{version}-macos-#{arch}.zip"
+  name "DNS Reset for Tunnelblick"
+  desc "Menu bar network recovery after Tunnelblick hangs"
+  homepage "https://github.com/viktoraseev/dns-reset-tunnelblick"
+
+  depends_on macos: ">= :ventura"
+
+  app "DNS Reset.app"
+end
---
+++ README.md
@@ -0,0 +1,10 @@
+# Viktor Aseev's Homebrew tap
+
+Общий tap для моих приложений. Сейчас доступен [DNS Reset for Tunnelblick](https://github.com/viktoraseev/dns-reset-tunnelblick).
+
+```sh
+brew tap viktoraseev/homebrew https://github.com/viktoraseev/homebrew.git
+brew install --cask viktoraseev/homebrew/dns-reset-tunnelblick
+```
+
+Приложение пока не заверено у Apple. Инструкции для первого запуска и настройки Touch ID находятся в [README проекта](https://github.com/viktoraseev/dns-reset-tunnelblick#readme).
