e6edf02cdf231996607158a98572479f5cc764f5
viktoraseev <viktoraseev@yandex-team.ru>
2026-09-17 09:38:04 +0300

    Fix Homebrew cask style

--- Casks/dns-reset-tunnelblick.rb
+++ Casks/dns-reset-tunnelblick.rb
@@ -1,7 +1,7 @@
 cask "dns-reset-tunnelblick" do
-  version "1.0.0"
   arch arm: "arm64", intel: "x86_64"
 
+  version "1.0.0"
   sha256 arm:   "8703c18e7b86dff22c643db8a9ca70fce058a96304da89cb0d2e9ef11ca9b6fa",
          intel: "eb12f22f9b41b36325a338bd9dafa6c57dd08eeefb26a0d1b23840b4da68effd"
 
@@ -10,7 +10,7 @@ cask "dns-reset-tunnelblick" do
   desc "Menu bar network recovery after Tunnelblick hangs"
   homepage "https://github.com/viktoraseev/dns-reset-tunnelblick"
 
-  depends_on macos: ">= :ventura"
+  depends_on macos: :ventura
 
   app "DNS Reset.app"
 end
